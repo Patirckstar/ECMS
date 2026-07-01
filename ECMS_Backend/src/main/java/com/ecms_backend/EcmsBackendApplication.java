0cc26155ee628888f4c792b6d5013bd63fe7aaf4
Patrickstar <15289529102@163.com>
2026-07-01 16:39:17 +0800

    这次我们前后端都已经创建好了，但依然是一个空项目

---
+++ ECMS_Backend/src/main/java/com/ecms_backend/EcmsBackendApplication.java
@@ -0,0 +1,13 @@
+package com.ecms_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcmsBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EcmsBackendApplication.class, args);
+    }
+
+}
---
+++ ECMS_Backend/src/test/java/com/ecms_backend/EcmsBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ecms_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcmsBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
